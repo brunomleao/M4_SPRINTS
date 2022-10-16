c9ee20336605be101d7ea3d349bedf08460b2b11
Bruno Leão <99328889+brunomleao@users.noreply.github.com>
2022-10-16 18:13:01 -0300

    Exercício não finalizado

--- SEMANA_01/EXERCICIOS/EX1/DEV/main.cpp
+++ SEMANA_01/EXERCICIOS/EX1/DEV/main.cpp
@@ -1,19 +1,53 @@
 #include <iostream>
 #include <string>
 
+using namespace std;
 // 1 -  Faça uma função que recebe uma certa medida e ajusta ela percentualmente 
 // entre dois valores mínimo e máximo e retorna esse valor
+void ajuste(){
+    int minimo = 0;
+    int maximo = 0;
+    float valor = 0;
+    cout<<"Insira aqui o valor minimo(intervalo inferior)!";
+    cin >> minimo;
+    cout<<"Insira aqui o valor máximo(intervalo superior)!";
+    cin >> maximo;
+    cout<<"Insira aqui o valor a ser ajustado percentualmente!";
+    cin >> valor;
 
+    if (valor>maximo || valor<minimo) {
+        cout<<"Valor inserido está fora do intervalo!\n";
+    }
+    else {
+        float ajuste = ((valor - minimo)/(maximo - minimo))*100;
+        cout<< "O valor "<< valor<<" ajustado percentualmente no intervalo [" << minimo <<","<<maximo <<"]"<<" será = "<< ajuste<<"%"<<"\n";
+    }
+}
 // 2 - Faça uma função que simule a leitura de um sensor lendo o 
 // valor do teclado ao final a função retorna este valor
+void scanner(){
+int contador = 1;
+char valor = 0;
+    
+    cout <<"Pressione em algum botão do teclado numérico para o sensor ler e o programa retornar esse valor;\n";
+    cin>>valor;
+    cout<<"Valor lido pelo sensor: "<<valor<<"\n";
 
+    
+}
 // 3 - Faça uma função que armazena uma medida inteira qualquer 
 // em um vetor fornecido. Note que como C não possui vetores 
 // nativos da linguagem, lembre-se que você precisa passar o 
 // valor máximo do vetor assim como a última posição preenchida
 // Evite também que, por acidente, um valor seja escrito em 
 // uma área de memória fora do vetor
-
+void vetor(){
+    int i = 0;
+    cout<<"Insira a medida inteira a ser armazenada em um vetor: ";
+    cin>>i;
+    int vetor[]={i};
+    cout<<vetor[0];
+}
 
 
 // 4 - Faça uma função que recebe um vetor com 4 posições que contém 
@@ -21,13 +55,40 @@
 // A função deve retornar duas informações: A primeira é a direção 
 // de maior distância ("Direita", "Esquerda", "Frente", "Tras") e a 
 // segunda é esta maior distância.
-
+void posicoes(){
+    string posicao[4] = {"direita", "esquerda", "frente", "tras"};
+    int distancia[4];
+    int max_index = 0;
+    for(int i=0;i<4;i++){
+        cout<<"Insira a distancia do robo para seu lado da "<<posicao[i]<<"\n";
+        cin>>distancia[i];
+    }
+    int max = distancia[0];
+    for(int i=0;i<4;i++){
+        if(distancia[i]>max){
+            max = distancia[i];
+            max_index = i;
+        }
+    }
+    cout<<"A direção de maior distância corresponde a: "<<posicao[max_index]<< " e ela corresponde a: "<<distancia[max_index]<<"\n";
 
 
 
 // 5 - Faça uma função que pergunta ao usuário se ele deseja continuar o mapeamento e 
 // retorna verdadeiro ou falso
-
+int continuar(){
+    char valor;
+    cout<<"Deseja continuar o mapeamento? Digite S para sim e N para não.\n";
+    cin>>valor;
+    if(valor=='S') {
+        cout<<"Se deseja continuar!\n";
+        return 1;
+    }
+    else {
+        cout<<"Se deseja encerrar!\n";
+        return 0;
+    }
+}
 
 // 6 - A função abaixo (que está incompleta) vai "dirigindo" virtualmente um robô 
 // e através de 4 sensores em cada um dos 4 pontos do robo ("Direita", "Esquerda", 
